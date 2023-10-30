56382a8fadc2307ae2fa9dcc4e522077c5626fc4
Stefaan Lippens <stefaan.lippens@vito.be>
2023-10-30 18:11:27 +0100

    Issue #4 initial experiments with dynamic test generation from json

--- src/openeo_test_suite/tests/processes/processing/test_example.py
+++ src/openeo_test_suite/tests/processes/processing/test_example.py
@@ -1,3 +1,7 @@
+import pytest
+import requests
+
+
 def test_add35(connection):
     pg = {
         "add35": {
@@ -8,3 +12,26 @@ def test_add35(connection):
     }
     response = connection.execute(pg)
     assert response == 8
+
+
+def get_examples(url):
+    data = requests.get(url).json()
+    return data["tests"]
+
+
+@pytest.mark.parametrize(
+    "example",
+    get_examples(
+        "https://raw.githubusercontent.com/Open-EO/openeo-processes/add-tests/tests/add.json5"
+    ),
+)
+def test_add(connection, example):
+    pg = {
+        "add": {
+            "process_id": "add",
+            "arguments": example["arguments"],
+            "result": True,
+        }
+    }
+    response = connection.execute(pg)
+    assert response == example["returns"]
