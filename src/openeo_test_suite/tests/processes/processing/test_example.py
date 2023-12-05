2b1e5a0fbc1e82d6ad59439b63cd1c5f105f63f4
Matthias Mohr <webmaster@mamo-net.de>
2023-12-05 16:43:58 +0100

    Cache requests, fix example builder, make assertions better

--- src/openeo_test_suite/tests/processes/processing/test_example.py
+++ src/openeo_test_suite/tests/processes/processing/test_example.py
@@ -1,20 +1,24 @@
+import math
 import pytest
-import requests
+import requests_cache
+import warnings
+
+spec_url = "https://raw.githubusercontent.com/Open-EO/openeo-processes/add-tests/tests/{}.json5"
 
 def add_id(test, id):
-    test["id"] = id
+    test["process_id"] = id
     return test
 
-def get_examples(url, ids):
+def get_examples(url_template, ids):
     examples = []
     for id in ids:
-        url = url.format(id)
+        session = requests_cache.CachedSession("openeo-processes-tests", expire_after=12*60*60) # cache for 12 hours
         try:
-            data = requests.get(url).json()
+            url = url_template.format(id)
+            data = session.get(url).json()
+            examples += map(lambda test: add_id(test, id), data["tests"])
         except:
-            print("No tests available for process " + id)
-            continue
-        examples += map(lambda test: add_id(test, id), data["tests"])
+            warnings.warn("No tests available for process " + id)
     
     return examples
 
@@ -24,7 +28,6 @@ def get_test_parameters():
     connection = dask_connection()
 
     process_ids = map(lambda process: process["id"], connection.list_processes())
-    spec_url = "https://raw.githubusercontent.com/Open-EO/openeo-processes/add-tests/tests/{}.json5"
     return pytest.mark.parametrize(
         "example", 
         get_examples(spec_url, process_ids)
@@ -36,12 +39,34 @@ def test_process(connection, example):
     pg = {
         "process_graph": {
             "node": {
-                "process_id": example["id"],
+                "process_id": example["process_id"],
                 "arguments": example["arguments"],
                 "result": True,
             }
         }
     }
 
-    response = connection.execute(pg)
-    assert response == example["returns"]
+    # todo: handle experimental processes (warning instead of error?)
+    experimental = example["experimental"] if "experimental" in example else False
+
+    if "throws" in example and not "returns" in example:
+        with pytest.raises(Exception):
+            # todo: check that the exception is of the right type (if example["throws"] is given as string)
+            result = connection.execute(pg)
+    else:
+        result = connection.execute(pg)
+
+    # todo: handle external references to files
+    # todo: handle labeled arrays
+    # todo: handle datacube metadata
+    if "throws" in example and "returns" in example:
+        # todo: handle exceptions correctly
+        warnings.warn("Can't handle throws and returns at the same time yet")
+    elif math.isnan(example["returns"]):
+        assert math.isnan(result)
+    elif isinstance(example["returns"], float):
+        assert isinstance(result, float)
+        delta = example["delta"] if "delta" in example else 0.0000000001
+        assert result == pytest.approx(example["returns"], delta)
+    else:
+        assert result == example["returns"]
