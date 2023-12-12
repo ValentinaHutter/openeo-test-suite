fd4cc098a5b2bee57ecbe052e12960617db95fee
Matthias Mohr <webmaster@mamo-net.de>
2023-12-12 17:41:31 +0100

    Remove VITO HTTP, can soon be accessed directly via normal HTTP

--- src/openeo_test_suite/tests/processes/processing/conftest.py
+++ src/openeo_test_suite/tests/processes/processing/conftest.py
@@ -67,10 +67,6 @@ def connection(backend_url: str, runner: str, auto_authenticate: bool, capfd) ->
     elif runner == "vito":
         from openeo_test_suite.lib.process_runner.vito import Vito
         return Vito()
-    elif runner == "vito-http":
-        from openeo_test_suite.lib.process_runner.vito_http import VitoHttp
-        con = openeo.connect(backend_url)
-        return VitoHttp(con)
     else:
         from openeo_test_suite.lib.process_runner.http import Http
         con = openeo.connect(backend_url)
