241f0ebf0cdfa51108882b1c0804af8998dc721f
Matthias Mohr <webmaster@mamo-net.de>
2023-12-13 12:33:29 +0100

    More reasonable dummy test for backend_url

--- src/openeo_test_suite/tests/test_dummy.py
+++ src/openeo_test_suite/tests/test_dummy.py
@@ -1,2 +1,9 @@
+from urllib.parse import urlparse
+
+
 def test_backend_url(backend_url):
-    assert backend_url
+    assert isinstance(backend_url, str) or backend_url is None
+    if backend_url is not None:
+        result = urlparse(backend_url)
+        assert result.scheme in ["http", "https"]
+        assert len(result.netloc) > 0
