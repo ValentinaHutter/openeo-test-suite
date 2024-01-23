b702cde6c6dbcda6235de30298b7bbf63954e973
Stefaan Lippens <stefaan.lippens@vito.be>
2024-01-23 11:51:31 +0100

    Add more internal tests (backend_under_test)

--- src/openeo_test_suite/lib/internal-tests/test_backend_under_test.py
+++ src/openeo_test_suite/lib/internal-tests/test_backend_under_test.py
@@ -2,7 +2,13 @@ from unittest import mock
 
 import pytest
 
-from openeo_test_suite.lib.backend_under_test import get_backend_url
+from openeo_test_suite.lib.backend_under_test import (
+    NoBackend,
+    get_backend_under_test,
+    get_backend_url,
+    get_collection_ids,
+    get_process_ids,
+)
 
 
 def test_get_backend_url_default(request):
@@ -22,3 +28,16 @@ def test_get_backend_url_mocked_config(configured_url, expected):
     config.getoption.return_value = configured_url
     url = get_backend_url(config=config, required=True)
     assert url == expected
+
+
+def test_get_backend_under_test():
+    but = get_backend_under_test()
+    assert isinstance(but, NoBackend)
+
+
+def test_get_collection_ids():
+    assert get_collection_ids() == []
+
+
+def test_get_process_ids():
+    assert get_process_ids() == []
