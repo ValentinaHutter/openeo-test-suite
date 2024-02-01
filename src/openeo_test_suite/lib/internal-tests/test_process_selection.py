88151b9af6e8679ddd59e210e05578dd0ea66752
Stefaan Lippens <stefaan.lippens@vito.be>
2024-02-01 19:51:22 +0100

    Fix caching issue with get_selected_processes()
    a non-reusable generator was being cached instead of a cachable concrete result (list)
    
    related to PR #44

--- src/openeo_test_suite/lib/internal-tests/test_process_selection.py
+++ src/openeo_test_suite/lib/internal-tests/test_process_selection.py
@@ -1,4 +1,4 @@
-from openeo_test_suite.lib.process_selection import csv_to_list
+from openeo_test_suite.lib.process_selection import csv_to_list, get_selected_processes
 
 
 def test_csv_to_list():
@@ -18,3 +18,12 @@ def test_csv_to_list_none_on_empty():
     assert csv_to_list("", none_on_empty=True) is None
     assert csv_to_list("   ", none_on_empty=True) is None
     assert csv_to_list(" ,  ", none_on_empty=True) is None
+
+
+def test_get_selected_processes_caching():
+    processes = get_selected_processes()
+    assert len(processes) > 0
+
+    processes2 = get_selected_processes()
+    assert processes2 is processes
+    assert len(processes2) > 0
--- src/openeo_test_suite/lib/process_registry.py
+++ src/openeo_test_suite/lib/process_registry.py
@@ -91,7 +91,7 @@ class ProcessRegistry:
         process_ids: Optional[List[str]] = None,
         process_levels: Optional[List[str]] = None,
         experimental: bool = False,
-    ) -> Iterable[ProcessData]:
+    ) -> Iterator[ProcessData]:
         """
         Collect processes matching with additional filtering:
 
